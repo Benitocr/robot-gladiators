9c91d1b56164ce1c580a37286f1d5bd5cc3c38ad
Benito <benitocavazosr@yahoo.com>
2021-11-17 23:26:21 -0600

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,10 +10,25 @@ var playerMoney = 10;
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack =  12;
-//Alert the player the game just begun.  
-window.alert("Welcome to Robot Gladiatiors!");
-var fight = function(enemyName){
+if (playerHealth > 0){
+  // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+  window.alert("Welcome to Robot Gladiators! Round " + (i+1));
+  
+    // pick new enemy to fight based on the index of the enemyNames array
+  var pickedEnemyName = enemyNames[i];
+  //reset enemyHealth before starting new fight
+  enemyHealth = 50;
+  // use debugger to pause script from runnin and check whats going on at the moment in the code
   debugger;
+  //pass the pickedEnemyName variables value into the fight fuction, where it will assume the value of the enemyName parameter
+  fight(pickedEnemyName);
+}else{
+  window.alert("You have lost your robot in battle! Game Over!");
+  
+};
+
+var fight = function(enemyName){
+ 
   while(playerHealth > 0 && enemyHealth > 0){  
     //ask player if choses to fight or not
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -59,11 +74,11 @@ var fight = function(enemyName){
           break;
         } else {
           window.alert(playerName + " still has " + playerHealth + " health left.");
-        }
+        };
        
-      // } 
+      
     
-  }
+  };
 
 
 };
